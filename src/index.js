9f86303acd29d77f0641174a8ae602a2c68cea3e
Jutawuth Nantawan <zeazons@gmail.com>
2020-01-31 10:57:52 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Emulator from "./components/Emulator";
+
 export default class EmulatorComponent extends Component {
   render() {
     return <div> I am a Emulator react npm module </div>;
