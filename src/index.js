d110c186aa07fde50ef68521ebe5b32792733daf
Jutawuth Nantawan <zeazons@gmail.com>
2020-01-31 17:29:27 +0700

    commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Emulator from "./components/Emulator";
-
-import FieldsMapping from "./components/FieldsMapping";
-import ListMapping from "./components/ListMapping";
-import ScreenMapping from "./components/ScreenMapping";
-
-export default [Emulator, FieldsMapping, ListMapping, ScreenMapping];
