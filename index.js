846c970beed8d99bc47130f61d6ecb211e58b6f2
Jutawuth Nantawan <zeazons@gmail.com>
2020-01-31 17:05:03 +0700

    commit

--- index.js
+++ index.js
@@ -415,29 +415,85 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
-var _default = EmulatorView = function EmulatorView(props) {
-  var id = props.id,
-      readOnly = props.readOnly;
-  return _react["default"].createElement("div", {
-    className: "as400-container"
-  }, _react["default"].createElement("div", {
-    className: "screen"
-  }, _react["default"].createElement("div", {
-    className: "".concat(id, " as400-highlight")
-  }), _react["default"].createElement("div", {
-    className: "".concat(id, " as400-style")
-  }), _react["default"].createElement("textarea", {
-    readOnly: readOnly ? readOnly : false,
-    className: "".concat(id, " as400-screen")
-  }), _react["default"].createElement("textarea", {
-    readOnly: readOnly ? readOnly : false,
-    className: "".concat(id, " as400-screen-temp")
-  })));
-};
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var EmulatorView =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(EmulatorView, _Component);
+
+  function EmulatorView() {
+    _classCallCheck(this, EmulatorView);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(EmulatorView).apply(this, arguments));
+  }
+
+  _createClass(EmulatorView, [{
+    key: "render",
+    value: function render() {
+      return _react["default"].createElement("div", {
+        className: "as400-container"
+      }, _react["default"].createElement("div", {
+        className: "screen"
+      }, _react["default"].createElement("div", {
+        className: "".concat(id, " as400-highlight")
+      }), _react["default"].createElement("div", {
+        className: "".concat(id, " as400-style")
+      }), _react["default"].createElement("textarea", {
+        readOnly: readOnly ? readOnly : false,
+        className: "".concat(id, " as400-screen")
+      }), _react["default"].createElement("textarea", {
+        readOnly: readOnly ? readOnly : false,
+        className: "".concat(id, " as400-screen-temp")
+      })));
+    }
+  }]);
+
+  return EmulatorView;
+}(_react.Component);
+
+var _default = EmulatorView; // import React from "react";
+// export default EmulatorView = props => {
+//   const { id, readOnly } = props;
+//   return (
+//     <div className="as400-container">
+//       <div className="screen">
+//         <div className={`${id} as400-highlight`}></div>
+//         <div className={`${id} as400-style`}></div>
+//         <textarea
+//           readOnly={readOnly ? readOnly : false}
+//           className={`${id} as400-screen`}
+//         ></textarea>
+//         <textarea
+//           readOnly={readOnly ? readOnly : false}
+//           className={`${id} as400-screen-temp`}
+//         ></textarea>
+//       </div>
+//     </div>
+//   );
+// };
 
 exports["default"] = _default;
 "use strict";
@@ -1023,6 +1079,8 @@ exports["default"] = void 0;
 
 var _Emulator = _interopRequireDefault(require("./components/Emulator"));
 
+var _EmulatorView = _interopRequireDefault(require("./components/EmulatorView"));
+
 var _FieldsMapping = _interopRequireDefault(require("./components/FieldsMapping"));
 
 var _ListMapping = _interopRequireDefault(require("./components/ListMapping"));
@@ -1031,5 +1089,5 @@ var _ScreenMapping = _interopRequireDefault(require("./components/ScreenMapping"
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _default = [_Emulator["default"], _FieldsMapping["default"], _ListMapping["default"], _ScreenMapping["default"]];
+var _default = [_Emulator["default"], _EmulatorView["default"], _FieldsMapping["default"], _ListMapping["default"], _ScreenMapping["default"]];
 exports["default"] = _default;
--- src/components/EmulatorView/index.jsx
+++ src/components/EmulatorView/index.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default EmulatorView = props => {
-  const { id, readOnly } = props;
-
-  return (
-    <div className="as400-container">
-      <div className="screen">
-        <div className={`${id} as400-highlight`}></div>
-        <div className={`${id} as400-style`}></div>
-        <textarea
-          readOnly={readOnly ? readOnly : false}
-          className={`${id} as400-screen`}
-        ></textarea>
-        <textarea
-          readOnly={readOnly ? readOnly : false}
-          className={`${id} as400-screen-temp`}
-        ></textarea>
+class EmulatorView extends Component {
+  render() {
+    return (
+      <div className="as400-container">
+        <div className="screen">
+          <div className={`${id} as400-highlight`}></div>
+          <div className={`${id} as400-style`}></div>
+          <textarea
+            readOnly={readOnly ? readOnly : false}
+            className={`${id} as400-screen`}
+          ></textarea>
+          <textarea
+            readOnly={readOnly ? readOnly : false}
+            className={`${id} as400-screen-temp`}
+          ></textarea>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default EmulatorView;
+
+// import React from "react";
+
+// export default EmulatorView = props => {
+//   const { id, readOnly } = props;
+
+//   return (
+//     <div className="as400-container">
+//       <div className="screen">
+//         <div className={`${id} as400-highlight`}></div>
+//         <div className={`${id} as400-style`}></div>
+//         <textarea
+//           readOnly={readOnly ? readOnly : false}
+//           className={`${id} as400-screen`}
+//         ></textarea>
+//         <textarea
+//           readOnly={readOnly ? readOnly : false}
+//           className={`${id} as400-screen-temp`}
+//         ></textarea>
+//       </div>
+//     </div>
+//   );
+// };
