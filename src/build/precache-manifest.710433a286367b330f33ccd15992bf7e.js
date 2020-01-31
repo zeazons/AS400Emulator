3009154110ed0051e84e5c6f2bca378be2a84a5d
Jutawuth Nantawan <zeazons@gmail.com>
2020-01-31 13:51:51 +0700

    commit

---
+++ src/build/precache-manifest.710433a286367b330f33ccd15992bf7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2725c3194f5499226b56e53114840b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7f20a55915d9897350",
+    "url": "/static/css/main.3e32d66b.chunk.css"
+  },
+  {
+    "revision": "3b6d57a9967e7318cdd6",
+    "url": "/static/js/2.04e2c1b5.chunk.js"
+  },
+  {
+    "revision": "e1317a2b34d7e85b6f18acb5bd0b1306",
+    "url": "/static/js/2.04e2c1b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e7f20a55915d9897350",
+    "url": "/static/js/main.9f68ed3d.chunk.js"
+  },
+  {
+    "revision": "5c3556a4d602d8503993",
+    "url": "/static/js/runtime-main.020cb127.js"
+  }
+]);
